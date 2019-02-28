e2eff6dc4d29b23c5fdd09e9f1c250e1e989d9b6
anarghya <anarghya@buffalo.edu>
2019-02-28 16:34:00 -0500

    :art: new changes

--- rgbGUI.py
+++ rgbGUI.py
@@ -10,26 +10,30 @@ f.grid(row=1,column=0,sticky="NW")
 f.grid_propagate(0)
 f.update()
 
+# * Title Label 
+title = Label(f,text="RGB Viewer")
+title.config(font=("Roboto Slab", 20))
+title.place(x=400, y=50, anchor="center")
+
+colors=getRGB()
+
+red=Label(f,text="RED: "+str(colors[0]))
+red.config(font=("Roboto Slab", 15))
+red.place(x=400, y=100, anchor="center")
+
+green=Label(f,text="GREEN: "+str(colors[1]))
+green.config(font=("Roboto Slab", 15))
+green.place(x=400, y=150, anchor="center")
+
+blue=Label(f,text="BLUE: "+str(colors[2]))
+blue.config(font=("Roboto Slab", 15))
+blue.place(x=400, y=200, anchor="center")
+
 def make():
-    # * Title Label 
-    title = Label(f,text="RGB Viewer")
-    title.config(font=("Roboto Slab", 20))
-    title.place(x=400, y=50, anchor="center")
-
     colors=getRGB()
-
     red=Label(f,text="RED: "+str(colors[0]))
-    red.config(font=("Roboto Slab", 15))
-    red.place(x=400, y=100, anchor="center")
-
-    green=Label(f,text="Green: "+str(colors[1]))
-    green.config(font=("Roboto Slab", 15))
-    green.place(x=400, y=150, anchor="center")
-
+    green=Label(f,text="GREEN: "+str(colors[1]))
     blue=Label(f,text="BLUE: "+str(colors[2]))
-    blue.config(font=("Roboto Slab", 15))
-    blue.place(x=400, y=200, anchor="center")
-
     app.after(1000,make)
 
 make()
