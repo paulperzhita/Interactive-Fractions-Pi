a9f90e351088a1fe0eed71f37f0708ad4a54a035
anarghya <anarghya@buffalo.edu>
2019-02-28 13:45:59 -0500

    :art: added comments and refeactor gui file

--- newGUI.py
+++ newGUI.py
@@ -1,21 +1,32 @@
 from tkinter import Tk,Frame,Grid, Label, Button
 
 app = Tk()
-app.title("Interactive Fractions")
+app.title("Interactive Fractions") # * Name of the window 
+
+# * Main frame on which everything is added 
 f = Frame(app,width=500,height=500)
 f.grid(row=1,column=0,sticky="NW")
 f.grid_propagate(0)
 f.update()
+
+# * Title Label 
 title = Label(f,text="Welcome To Interactive Fractions")
 title.config(font=("Roboto Slab", 20))
 title.place(x=250, y=50, anchor="center")
+
+# * Question Label
 question = Label(f,text="Question comes here")
 question.config(font=("Roboto Slab", 15))
 question.place(x=250, y=100, anchor="center")
+
+# * Submit Button
 submit=Button(f,text="Submit")
 submit.config(font=("Roboto Slab", 60))
 submit.place(x=250, y=220, anchor="center")
-question = Label(f,text="Hint comes here")
-question.config(font=("Roboto Slab", 15))
-question.place(x=250, y=360, anchor="center")
-app.mainloop()
+
+# * Hint Label
+hint = Label(f,text="Hint comes here")
+hint.config(font=("Roboto Slab", 15))
+hint.place(x=250, y=360, anchor="center")
+
+app.mainloop() # * Starts the GUI
