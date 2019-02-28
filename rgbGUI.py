5a3a46f65daf5e8dc1aef48a4b4f8e5d3c389a56
anarghya <anarghya@buffalo.edu>
2019-02-28 16:27:07 -0500

    :sparkles: new changes

--- rgbGUI.py
+++ rgbGUI.py
@@ -1,5 +1,5 @@
 from tkinter import Tk,Frame,Grid, Label, Button
-# from rbgSensor import getRGB
+from rbgSensor import getRGB
 
 app = Tk()
 app.title("RGB GUI") # * Name of the window 
@@ -10,23 +10,28 @@ f.grid(row=1,column=0,sticky="NW")
 f.grid_propagate(0)
 f.update()
 
-# * Title Label 
-title = Label(f,text="RGB Viewer")
-title.config(font=("Roboto Slab", 20))
-title.place(x=400, y=50, anchor="center")
+def make():
+    # * Title Label 
+    title = Label(f,text="RGB Viewer")
+    title.config(font=("Roboto Slab", 20))
+    title.place(x=400, y=50, anchor="center")
 
-colors=getRGB()
+    colors=getRGB()
 
-red=Label(f,text="RED: "+str(colors[0]))
-red.config(font=("Roboto Slab", 15))
-red.place(x=400, y=100, anchor="center")
+    red=Label(f,text="RED: "+str(colors[0]))
+    red.config(font=("Roboto Slab", 15))
+    red.place(x=400, y=100, anchor="center")
 
-green=Label(f,text="Green: "+str(colors[1]))
-green.config(font=("Roboto Slab", 15))
-green.place(x=400, y=150, anchor="center")
+    green=Label(f,text="Green: "+str(colors[1]))
+    green.config(font=("Roboto Slab", 15))
+    green.place(x=400, y=150, anchor="center")
 
-blue=Label(f,text="BLUE: "+str(colors[2]))
-blue.config(font=("Roboto Slab", 15))
-blue.place(x=400, y=200, anchor="center")
+    blue=Label(f,text="BLUE: "+str(colors[2]))
+    blue.config(font=("Roboto Slab", 15))
+    blue.place(x=400, y=200, anchor="center")
+
+    app.after(1000,make)
+
+make()
 
 app.mainloop()
