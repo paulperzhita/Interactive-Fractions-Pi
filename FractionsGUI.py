2aad6404a1626e606ce9bb7aac97cb3a8ccf4508
anarghya <anarghya@buffalo.edu>
2019-02-28 17:24:26 -0500

    :art: added color

--- FractionsGUI.py
+++ FractionsGUI.py
@@ -108,16 +108,12 @@ question = Label(window, text = "")
 changeQuestion()
 question.grid(row = 0, column = 0)
 
-while(True):
 
-    denominatorButton = Button(window, text = "Confirm Common Denominator", command = checkCommonDenominator)
-    denominatorButton.grid(row = 1, column = 0)
-
-    answerButton = Button(window, text = "Confirm Answer", command = checkAnswer, width = 20)
-    reducedAnswerButton = Button(window, text = "Confirm Reduced Answer", command = checkReducedAnswer, width = 20)
-
-    feedback = Label(window, text = "") 
-    feedback.grid(row = 2, column = 0)
-
-    window.mainloop()
+denominatorButton = Button(window, text = "Confirm Common Denominator", command = checkCommonDenominator)
+denominatorButton.grid(row = 1, column = 0)
+answerButton = Button(window, text = "Confirm Answer", command = checkAnswer, width = 20)
+reducedAnswerButton = Button(window, text = "Confirm Reduced Answer", command = checkReducedAnswer, width = 20)
+feedback = Label(window, text = "") 
+feedback.grid(row = 2, column = 0)
+window.mainloop()
 
--- rgbGUI.py
+++ rgbGUI.py
@@ -1,4 +1,4 @@
-from tkinter import Tk,Frame,Grid, Label, Button
+from tkinter import Tk,Frame,Grid, Label, Button, Canvas
 from rbgSensor import getRGB
 
 app = Tk()
@@ -9,6 +9,11 @@ f = Frame(app,width=800,height=500)
 f.grid(row=1,column=0,sticky="NW")
 f.grid_propagate(0)
 f.update()
+
+def _from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    return "#%02x%02x%02x" % rgb
 
 # * Title Label 
 title = Label(f,text="RGB Viewer")
@@ -29,11 +34,16 @@ blue=Label(f,text="BLUE: "+str(colors[2]))
 blue.config(font=("Roboto Slab", 15))
 blue.place(x=400, y=200, anchor="center")
 
+canvas=Canvas(f,width=200,height=200)
+canvas.place(x=400, y=250, anchor="center")
+canvas.create_rectangle(50, 25, 150, 75, fill=_from_rgb((colors[0],colors[1],colors[2])))
+
 def make():
     colors=getRGB()
     red.config(text="RED: "+str(colors[0]))
     green.config(text="GREEN: "+str(colors[1]))
     blue.config(text="BLUE: "+str(colors[2]))
+    canvas.create_rectangle(50, 25, 150, 75, fill=_from_rgb((colors[0],colors[1],colors[2])))
     app.after(1000,make)
 
 make()
