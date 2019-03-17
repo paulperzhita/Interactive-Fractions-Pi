c511e00059ba93d6d28ac2b08402a1a08fdd2496
anarghya <anarghya@buffalo.edu>
2019-03-17 14:39:16 -0400

    :art: added message is color not foundin scanned function

--- sensor.py
+++ sensor.py
@@ -18,63 +18,14 @@ values={  "Red":"1/8",
     "Pink":"1/12"
     }
 
-def getScannedFraction2(): # match the reading from the fraction circle to its corresponding color
-    
-    red = colorSensor.color_rgb_bytes[0]
-    green = colorSensor.color_rgb_bytes[1]
-    blue = colorSensor.color_rgb_bytes[2]
-
-    print(" ")
-    print("red: " + str(red))
-    print("green: " + str(green))
-    print("blue: " + str(blue))
-
-    if(red > 90 and green < 7 and blue < 6):
-        print("Color: Red")
-        return "1/8"
-    
-    elif(red < 10 and green > 5 and green < 20 and blue > 50):
-        print("Color: Blue")
-        return "1/1"
-    
-    elif(red > 5 and red < 15 and green > 10 and green < 20 and blue > 15 and blue < 25):
-        print("Color: Brown")
-        return "1/2"
-    
-    elif(red < 10 and green > 10 and green < 25 and blue > 20 and blue < 35):
-        print("Color: Black")
-        return "1/3"
-    
-    elif(red > 100 and green > 100 and blue > 30):
-        print("Color: Yellow")
-        return "1/4"
-    
-    elif(red < 5 and green > 40 and green < 55 and blue > 5 and blue < 15):
-        print("Color: Green")
-        return "1/5"
-    
-    elif(red > 70 and green < 10 and blue < 5 ):
-        print("Color: Orange")
-        return "1/6"
-    
-    elif(red > 10 and red < 30 and green < 15 and blue > 20 and blue < 40):
-        print("Color: Purple")
-        return "1/10"
-
-    else:
-        print("Color: Blue")
-        return "1/1"
-
 def getScannedFraction():
     colorName=rgbSensor.getRGB()[3]
     print(colorName)
-    return values[colorName]
+    if colorName is "Color Not Recognized":
+        return "-1"
+    else:
+        return values[colorName]
 
 
-i = 0
-while i < 10: #True:
-    print('Color: ({0}, {1}, {2})'.format(*colorSensor.color_rgb_bytes))
-    i = i + 1
-    #time.sleep(2)
 
 
